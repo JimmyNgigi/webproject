c37dae818b45d78a80de6d277835fb11f16fe30b
JimmyNgigi <jimmyngigi55@gmail.com>
2024-04-18 11:53:32 +0300

    (webpages)

--- webpages/models.py
+++ webpages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Myuser(models.Model):
+    FirstName = models.CharField(max_length=50)
+    LastName = models.CharField(max_length=50)
+    Email = models.EmailField(unique=True)
+    Age = models.IntegerField(null=True, blank=False)
+
